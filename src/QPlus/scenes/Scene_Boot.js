28da39005baf27f8c60f7caa64d4600ac478915c
inc0der <inc0der.code@gmail.com>
2020-10-08 10:27:01 -0300

    Fix quick start in QPlus (#11)

--- src/QPlus/scenes/Scene_Boot.js
+++ src/QPlus/scenes/Scene_Boot.js
@@ -1,5 +1,6 @@
 import { _PARAMS } from "../constants";
 
+//#if _MV
 const Alias_Scene_Boot_start = Scene_Boot.prototype.start;
 Scene_Boot.prototype.start = function () {
   if (DataManager.isBattleTest() || DataManager.isEventTest()) {
@@ -15,3 +16,18 @@ Scene_Boot.prototype.start = function () {
     Alias_Scene_Boot_start.call(this);
   }
 };
+
+//#else
+const Alias_Scene_Boot_startNormalGame = Scene_Boot.prototype.startNormalGame;
+
+Scene_Boot.prototype.startNormalGame = function () {
+  if (_PARAMS["Quick Test"] && Utils.isOptionValid("test")) {
+    Scene_Base.prototype.start.call(this);
+    this.checkPlayerLocation();
+    DataManager.setupNewGame();
+    SceneManager.goto(Scene_Map);
+  } else {
+    Alias_Scene_Boot_startNormalGame.call(this);
+  }
+};
+//#endif
