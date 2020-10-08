1e90e3b2b570d239bb2460e70628cca7bbe28818
inc0der <inc0der.code@gmail.com>
2020-10-08 09:08:06 -0300

    Fix the quick start option in QPlus for MZ

--- src/QPlus/scenes/Scene_Boot.js
+++ src/QPlus/scenes/Scene_Boot.js
@@ -1,17 +1,14 @@
 import { _PARAMS } from "../constants";
 
-const Alias_Scene_Boot_start = Scene_Boot.prototype.start;
-Scene_Boot.prototype.start = function () {
-  if (DataManager.isBattleTest() || DataManager.isEventTest()) {
-    Alias_Scene_Boot_start.call(this);
-  } else if (_PARAMS["Quick Test"] && Utils.isOptionValid("test")) {
+const Alias_Scene_Boot_startNormalGame = Scene_Boot.prototype.startNormalGame;
+
+Scene_Boot.prototype.startNormalGame = function () {
+  if (_PARAMS["Quick Test"] && Utils.isOptionValid("test")) {
     Scene_Base.prototype.start.call(this);
-    SoundManager.preloadImportantSounds();
     this.checkPlayerLocation();
     DataManager.setupNewGame();
     SceneManager.goto(Scene_Map);
-    this.updateDocumentTitle();
   } else {
-    Alias_Scene_Boot_start.call(this);
+    Alias_Scene_Boot_startNormalGame.call(this);
   }
 };
