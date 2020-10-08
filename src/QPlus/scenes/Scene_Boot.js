dc982548565cc319a14715dba4673c33085b06ba
inc0der <inc0der.code@gmail.com>
2020-10-08 10:24:33 -0300

    Add MV compatible quick start with conditional compilation

--- src/QPlus/scenes/Scene_Boot.js
+++ src/QPlus/scenes/Scene_Boot.js
@@ -1,5 +1,23 @@
 import { _PARAMS } from "../constants";
 
+//#if _MV
+const Alias_Scene_Boot_start = Scene_Boot.prototype.start;
+Scene_Boot.prototype.start = function () {
+  if (DataManager.isBattleTest() || DataManager.isEventTest()) {
+    Alias_Scene_Boot_start.call(this);
+  } else if (_PARAMS["Quick Test"] && Utils.isOptionValid("test")) {
+    Scene_Base.prototype.start.call(this);
+    SoundManager.preloadImportantSounds();
+    this.checkPlayerLocation();
+    DataManager.setupNewGame();
+    SceneManager.goto(Scene_Map);
+    this.updateDocumentTitle();
+  } else {
+    Alias_Scene_Boot_start.call(this);
+  }
+};
+
+//#else
 const Alias_Scene_Boot_startNormalGame = Scene_Boot.prototype.startNormalGame;
 
 Scene_Boot.prototype.startNormalGame = function () {
@@ -12,3 +30,4 @@ Scene_Boot.prototype.startNormalGame = function () {
     Alias_Scene_Boot_startNormalGame.call(this);
   }
 };
+//#endif
