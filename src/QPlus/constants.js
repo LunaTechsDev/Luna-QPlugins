48f736cf8607af1a7b73cdfc0299681d131c97aa
inc0der <inc0der.code@gmail.com>
2020-10-08 09:06:29 -0300

    Fix typo when getting params (#9)

--- src/QPlus/constants.js
+++ src/QPlus/constants.js
@@ -1,6 +1,6 @@
 import QPlus from "./QPlus";
 
-export const _PARAMS = QPlus.getParams("<LunaQPlus>", {
+export const _PARAMS = QPlus.getParams("<Luna_QPlus>", {
   "Quick Test": false,
   "Default Enabled Switches": [],
   "Ignore Mouse when inactive": false,
